e95732cab0aab2311584fd8fa45a47c7f4af0040
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2022-10-29 13:48:06 -0300

    Update main.py
    tratamento para salvar em CSV funcionando.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from transformers import BertTokenizer
 import requests
 import re
 import csv
+import json
+from collections import namedtuple
 
 
 def open_file(path):
@@ -18,31 +20,38 @@ def extract_cep_from_text(text):
         return cep
 
 
-def call_cep_api(cep):
+def call_cep_api(cep, lista):
     url = "http://viacep.com.br/ws/" + cep + "/json/"
     body = {}
     response = requests.request(url=url, params=body, method="get")
-    print(response.json())
+    x = response.json()
+    for i in x:
+        if (i == 'cep'):
+            # print("encontrado cep")
+            lista.append(x);
+        if (i == 'erro'):
+            print("cep com erro: " + cep)
 
+    # print(x)
 
-def write_csv():
-    f = open('numero_dobro_triplo.csv', 'w', newline='', encoding='utf-8')
-
-    # 2. cria o objeto de gravação
-    w = csv.writer(f)
-
-    # 3. grava as linhas
-    for i in range(5):
-        w.writerow([i, i * 2, i * 3])
-
+def write_csv(lista):
+    fieldnames = [*lista[0]]
+    with open('dados_enderecos.csv', 'w', encoding='UTF8', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(lista)
 
 def main():
     content = open_file("./data_enderecos.txt")
+    f = open('./endereco.csv', 'w', newline='', encoding='utf-8')
+    lista = []
+
     for line in content:
         cep = extract_cep_from_text(line)
         if cep:
-            call_cep_api(cep[0])
+            call_cep_api(cep[0], lista)
 
+    write_csv(lista)
 
 def tokeninze(itens):
     PRE_TRAINED_MODEL_NAME = 'neuralmind/bert-base-portuguese-cased'
